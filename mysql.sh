14e91e817299ebef86f128faded71a322510f4ae
Jeeva <endlurijeeva93@mail.com>
2025-10-15 20:05:53 +0530

    session-06

---
+++ mysql.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+source ./common.sh
+
+check_root
+
+dnf install mysql-server -y &>>$LOG_FILE
+VALIDATE $? "Installing MySQL Server"
+
+systemctl enable mysqld &>>$LOG_FILE
+systemctl start mysqld
+VALIDATE $? "Enabling and STarting MYSQL Server"
+
+mysql_secure_installation --set-root-pass RoboShop@1 &>>$LOG_FILE
+VALIDATE $? "Setting root password to MYSQL Server"
+print_total_time
